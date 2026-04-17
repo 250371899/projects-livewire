9b647d45409e264b52e1359f7ba318b9712cde22
250371899@aston.ac.uk <250371899@aston.ac.uk>
2026-04-17 21:08:53 +0100

    updated icon to public

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,7 @@
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
                 <div class="flex justify-center">
                   
-                <img src="{{ asset('icons/logo.png') }}" class="h-100 w-auto" alt="Logo">
+                <img src="icons/logo.png" class="h-100 w-auto" alt="Logo">
                 </div>
 
 
